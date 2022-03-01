f894b6b34f2f4a264bd6fe77bfb709bec5a2fc3a
bjterry99 <bjterry99@gmail.com>
2022-02-28 20:47:37 -0700

    mission9videos

---
+++ Mission7/Models/Purchase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Models
+{
+    public class Purchase
+    {
+        [Key]
+        [BindNever]
+        public int PurchaseId { get; set; }
+
+        [BindNever]
+        public ICollection<BasketLineItem> Lines {get;set;}
+
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter address line 1.")]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter a city.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter a state.")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter a country.")]
+        public string Country { get; set; }
+        public bool Anonymous { get; set; }
+    }
+}
