c0cfb3ccf743931d18177a069deb5b0f82f5b6a3
bjterry99 <bjterry99@gmail.com>
2022-03-07 14:14:56 -0700

    mission10

--- Mission7/Models/IBookstoreRepository.cs
+++ Mission7/Models/IBookstoreRepository.cs
@@ -9,5 +9,9 @@ namespace Mission7.Models
     {
         IQueryable<Book> Books { get; }
 
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
+
     }
 }
--- Mission7/Models/Purchase.cs
+++ Mission7/Models/Purchase.cs
@@ -32,5 +32,8 @@ namespace Mission7.Models
         [Required(ErrorMessage = "Please enter a country.")]
         public string Country { get; set; }
         public bool Anonymous { get; set; }
+
+        [BindNever]
+        public bool PurchaseReceived { get; set; }
     }
 }
